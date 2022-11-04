3a26db468a5cd1417236ac4adffdd5105744abbf
dav100ide <oreglio.dev@gmail.com>
2022-11-04 10:11:04 +0100

    live coding1

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@
 /* ==========================
       functions
 ============================ */
-
+/**
+ * funzione che crea il campo da gioco
+ * gridCells il numero delle celle
+ * gridContainer il container HTML che contiene la griglia
+ */
 function printGrid(gridCells = 100, gridContainer = document) {
    board.innerHTML = '';
 
@@ -11,21 +15,23 @@ function printGrid(gridCells = 100, gridContainer = document) {
       gridCell.innerHTML = i;
       gridCell.classList.add('board__number');
       // al click aggiungo la classe che mette un altro background-color
-      gridContainer.append(gridCell);
 
       gridCell.addEventListener('click', function () {
-         let clickCount = 0;
-         let result;
-         let message = document.getElementById('message');
          if (!bombs.includes(Number(this.innerHTML))) {
             this.classList.add('board__number-active');
          } else {
             this.classList.add('board__number-accent');
+            alert('hai perso la pag verrà ricaricata');
+            location.reload();
          }
       });
+      gridContainer.append(gridCell);
    }
 }
 
+/**
+ * funzione che genera un numero random intero nel range di min, max (inclusi)
+ */
 function getRndInteger(min, max) {
    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -36,9 +42,11 @@ function getRndInteger(min, max) {
 const board = document.querySelector('.board');
 const playBtn = document.getElementById('play-btn');
 const difficulty = document.getElementById('difficulty');
-
+const message = document.getElementById('message');
 let gridCells = 100;
+
 playBtn.addEventListener('click', function () {
+   // comportamento grafico in base alla difficoltà
    if (difficulty.value === 'hard') {
       gridCells = 49;
       document.documentElement.style.setProperty('--col-number', '7');
@@ -49,18 +57,16 @@ playBtn.addEventListener('click', function () {
       gridCells = 100;
       document.documentElement.style.setProperty('--col-number', '10');
    }
+
+   // creo un array di 16 bombe univoche
+   const bombs = [];
+
+   while (bombs.length < 16) {
+      const newNumber = getRndInteger(1, gridCells);
+      if (!bombs.includes(newNumber)) {
+         bombs.push(newNumber);
+      }
+   }
+   console.log(bombs);
    printGrid(gridCells, board);
 });
-
-printGrid(gridCells, board);
-
-// creo le bombe
-const bombs = [2, 5, 8, 15];
-
-// while (bombs.length < 16) {
-//    const newNumber = getRndInteger(1, gridCells);
-//    if (!bombs.includes(newNumber)) {
-//       bombs.push(newNumber);
-//    }
-// }
-console.log(bombs);
